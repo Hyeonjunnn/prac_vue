ad21d3716c8b127467b15fa0b5e482079af59415
MSP_31 <jhss9747@gmail.com>
2025-03-20 12:33:04 +0900

    [홍재민] 게시판 CRUD 수정

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,8 @@ import Admins from "@/pages/Admins.vue";
 import Login from "@/pages/Login.vue";
 import About from "@/pages/About.vue";
 import PostList from "@/pages/Post/PostList.vue";
-import AddPost from "@/pages/Post/AddPost.vue";
-import EditPost from "@/pages/Post/EditPost.vue";
-import PostDetail from "@/pages/Post/PostDetail.vue";
+import PostDeteil from "@/pages/Post/PostDeteil.vue";
+import PostWrite from "@/pages/Post/PostWrite.vue";
 import NotFound from "@/pages/NotFound.vue";
 import MessageCreate from "@/pages/Message/MessageCreate.vue";
 import MessageDetails from "@/pages/Message/MessageDetails.vue";
@@ -16,25 +15,23 @@ import ProjectList from "@/pages/Project/ProjectList.vue";
 import ProjectDiteil from "@/pages/Project/ProjectDiteil.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {path: "/", component: Home},
-        {path: "/login", component: Login},
-        {path: "/users", component: Users},
-        {path: "/admins", component: Admins},
-        {path: "/about", component: About},
-        {path: "/posts", component: PostList},
-        {path: "/posts/add", component: AddPost},
-        {path: "/posts/:id", component: EditPost},
-        {path: "/messages/list", component: MessageList},
-        {path: "/messages/write", component: MessageCreate},
-        {path: "/messages/:messageNo", component: MessageDetails},
-        {path: "/posts/:postNo", name: "EditPost", component: EditPost},
-        {path: "/posts/:postNo", name: "PostDetail", component: PostDetail},
-        {path: "/projects", name: "ProjectList", component: ProjectList},
-        {path: "/projects/:projectNo", name: "ProjectDetailt", component: ProjectDiteil},
-        {path: "/:paths(.*)*", component: NotFound},
-    ],
+  history: createWebHistory(),
+  routes: [
+    {path: "/", component: Home},
+    {path: "/login", component: Login},
+    {path: "/users", component: Users},
+    {path: "/admins", component: Admins},
+    {path: "/about", component: About},
+    {path: "/post", component: PostList},
+    {path: "/post/:postNo", component: PostDeteil},
+    {path: "/post/write", name: "PostWrite", component: PostWrite},
+    {path: "/messages/list", component: MessageList},
+    {path: "/messages/write", component: MessageCreate},
+    {path: "/messages/:messageNo", component: MessageDetails},
+    {path: "/projects", name: "ProjectList", component: ProjectList},
+    {path: "/projects/:projectNo", name: "ProjectDetailt", component: ProjectDiteil},
+    {path: "/:paths(.*)*", component: NotFound},
+  ],
 });
 
 export default router;
