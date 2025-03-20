1ccc25f28fea39f6faa1326ccd9a56c1904e59fb
DESKTOP-CEKNQTC <jhss9747@gmail.com>
2025-03-21 01:00:24 +0900

    [홍재민] 팀 CRUD 수정 중

--- src/pages/team/TeamDeteil.vue
+++ src/pages/team/TeamDeteil.vue
@@ -1,40 +1,40 @@
 <template>
-  <div>
-    <div class="max-w-7xl w-full mx-auto p-4 bg-white">
-      <!-- Author Section -->
-      <div class="flex items-center space-x-4 mb-6">
-        <img src="https://cdn.startupful.io/img/app_logo/no_img.png" alt="Author Avatar" class="w-12 h-12 rounded-full" />
-        <div>
-          <h3 class="font-semibold">{{ post.userName }}</h3>
-          <p class="text-gray-500 text-sm">
-            {{ new Date(post.createdAt).toLocaleDateString() }}
-          </p>
+    <div>
+        <div class="max-w-7xl w-full mx-auto p-4 bg-white">
+            <!-- Author Section -->
+            <div class="flex items-center space-x-4 mb-6">
+                <img src="https://cdn.startupful.io/img/app_logo/no_img.png" alt="Author Avatar" class="w-12 h-12 rounded-full" />
+                <div>
+                    <h3 class="font-semibold">{{ post.userName }}</h3>
+                    <p class="text-gray-500 text-sm">
+                        {{ new Date(post.createdAt).toLocaleDateString() }}
+                    </p>
+                </div>
+            </div>
+
+            <!-- Main Content -->
+            <div class="space-y-6">
+                <!-- 제목 -->
+                <h1 class="text-3xl font-bold text-black">
+                    {{ post.title }}
+                </h1>
+
+                <!-- 내용 -->
+                <div class="prose max-w-none text-black">
+                    <p class="text-gray-500 leading-relaxed">
+                        {{ post.content }}
+                    </p>
+                </div>
+            </div>
+
+            <!-- 수정 삭제 -->
+            <div>
+                <br />
+                <button class="px-3 py-1 text-sm text-white bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none" @click="goToEditPage">수정</button>
+                <button class="px-3 py-1 text-sm text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none" @click="confirmDelete(post.no)">삭제</button>
+            </div>
         </div>
-      </div>
-
-      <!-- Main Content -->
-      <div class="space-y-6">
-        <!-- 제목 -->
-        <h1 class="text-3xl font-bold text-black">
-          {{ post.title }}
-        </h1>
-
-        <!-- 내용 -->
-        <div class="prose max-w-none text-black">
-          <p class="text-gray-500 leading-relaxed">
-            {{ post.content }}
-          </p>
-        </div>
-      </div>
-
-      <!-- 수정 삭제 -->
-      <div>
-        <br />
-        <button class="px-3 py-1 text-sm text-white bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none" @click="goToEditPage">수정</button>
-        <button class="px-3 py-1 text-sm text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none" @click="confirmDelete(post.postNo)">삭제</button>
-      </div>
     </div>
-  </div>
 </template>
 <script>
 import {useRoute, useRouter} from "vue-router";
@@ -42,187 +42,190 @@ import {getUserInfo} from "@/utils/AuthUtil.js";
 import {ref} from "vue";
 import axios from "axios";
 export default {
-  name: "PostDiteil",
-  setup() {
-    // 초기 데이터 설정
-    const route = useRoute(); // 현재 URL 정보 가져옴
-    const router = useRouter(); // 경로 이동 (라우트를 이동하거나 상태 변경할때 사용)
-    const postNo = route.params.postNo; // 경로에 포함된 번호를 가져옴
-    const post = ref({}); // 반응형 데이터 선언
-    const initParams = {
-      page: 1,
-      size: 10,
-    };
+    name: "TeamDiteil",
+    setup() {
+        // 초기 데이터 설정
+        const route = useRoute(); // 현재 URL 정보 가져옴
+        const router = useRouter(); // 경로 이동 (라우트를 이동하거나 상태 변경할때 사용)
+        const teamNo = route.params.teamNo; // 경로에 포함된 번호를 가져옴
+        const post = ref({}); // 반응형 데이터 선언
+        const initParams = {
+            page: 1,
+            size: 10,
+        };
+        console.log(route);
+        console.log(route.params);
+        // 상세 정보 가져와서 post에 넣음
+        axios.get(`http://localhost:8087/team/${teamNo}`, initParams).then((response) => {
+            post.value = response.data;
+            console.log(post);
+            console.log(response);
+        });
 
-    // 상세 정보 가져와서 post에 넣음
-    axios.get(`http://localhost:8087/teams/${postNo}`, initParams).then((response) => {
-      post.value = response.data;
-    });
+        // 게시글 수정용 데이터
+        const goToEditPage = () => {
+            router.push({
+                name: "PostWrite",
+                query: {
+                    teamNo: post.value.teamNo,
+                    title: post.value.title,
+                    content: post.value.content,
+                    boardType: post.value.boardType,
+                },
+            });
+        };
 
-    // 게시글 수정용 데이터
-    const goToEditPage = () => {
-      router.push({
-        name: "PostWrite",
-        query: {
-          postNo: post.value.postNo,
-          title: post.value.title,
-          content: post.value.content,
-          boardType: post.value.boardType,
-        },
-      });
-    };
-
-    // 메소드 반환
-    return {
-      post,
-      goToEditPage,
-    };
-  },
-  methods: {
-    // 삭제 확인 창
-    confirmDelete(postNo) {
-      if (confirm("정말로 삭제하시겠습니까?")) {
-        // 삭제 처리 로직 호출
-        this.deletePostData(postNo);
-      }
+        // 메소드 반환
+        return {
+            post,
+            goToEditPage,
+        };
     },
-    // 게시글 삭제
-    async deletePostData(postNo) {
-      const url = `http://localhost:8087/posts/${postNo}`;
-      const token = getUserInfo().accessToken;
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + token,
+    methods: {
+        // 삭제 확인 창
+        confirmDelete(teamNo) {
+            if (confirm("정말로 삭제하시겠습니까?")) {
+                // 삭제 처리 로직 호출
+                this.deletePostData(teamNo);
+            }
         },
-      };
-      try {
-        const response = await axios.delete(url, config);
-        alert("삭제 됐습니다.");
-        // 게시글을 삭제한 후 기존 페이지로 돌려보냄
-        this.$router.push("/team");
-      } catch (error) {
-        alert(error.response.data.message);
-      }
+        // 게시글 삭제
+        async deletePostData(teamNo) {
+            const url = `http://localhost:8087/team/${teamNo}`;
+            const token = getUserInfo().accessToken;
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + token,
+                },
+            };
+            try {
+                const response = await axios.delete(url, config);
+                alert("삭제 됐습니다.");
+                // 게시글을 삭제한 후 기존 페이지로 돌려보냄
+                this.$router.push("/team");
+            } catch (error) {
+                alert(error.response.data.message);
+            }
+        },
     },
-  },
 };
 </script>
 
 <style scoped>
 /* Author Section */
 .flex {
-  display: flex;
+    display: flex;
 }
 
 .items-center {
-  align-items: center;
+    align-items: center;
 }
 
 .space-x-4 > * + * {
-  margin-left: 1rem;
+    margin-left: 1rem;
 }
 
 .mb-6 {
-  margin-bottom: 1.5rem;
+    margin-bottom: 1.5rem;
 }
 
 .w-12,
 .h-12 {
-  width: 3rem;
-  height: 3rem;
+    width: 3rem;
+    height: 3rem;
 }
 
 .rounded-full {
-  border-radius: 9999px;
+    border-radius: 9999px;
 }
 
 .font-semibold {
-  font-weight: 600;
+    font-weight: 600;
 }
 
 .text-gray-500 {
-  color: #6b7280;
+    color: #6b7280;
 }
 
 .text-sm {
-  font-size: 0.875rem;
+    font-size: 0.875rem;
 }
 
 /* Main Content */
 .space-y-6 > * + * {
-  margin-top: 1.5rem;
+    margin-top: 1.5rem;
 }
 
 .text-3xl {
-  font-size: 1.875rem;
+    font-size: 1.875rem;
 }
 
 .font-bold {
-  font-weight: 700;
+    font-weight: 700;
 }
 
 .text-black {
-  color: #000;
+    color: #000;
 }
 
 .prose p {
-  color: #6b7280;
-  line-height: 1.625;
+    color: #6b7280;
+    line-height: 1.625;
 }
 
 .max-w-none {
-  max-width: none;
+    max-width: none;
 }
 
 /* 수정 삭제 버튼 */
 .mt-4 {
-  margin-top: 1rem;
+    margin-top: 1rem;
 }
 
 .px-3,
 .py-1 {
-  padding: 0.25rem 0.75rem;
+    padding: 0.25rem 0.75rem;
 }
 
 .text-white {
-  color: #fff;
+    color: #fff;
 }
 
 .bg-indigo-600 {
-  background-color: #4f46e5;
+    background-color: #4f46e5;
 }
 
 .rounded-md {
-  border-radius: 0.375rem;
+    border-radius: 0.375rem;
 }
 
 .hover\:bg-indigo-500:hover {
-  background-color: #4338ca;
+    background-color: #4338ca;
 }
 
 .focus\:outline-none:focus {
-  outline: none;
+    outline: none;
 }
 
 .text-gray-700 {
-  color: #374151;
+    color: #374151;
 }
 
 .bg-gray-200 {
-  background-color: #e5e7eb;
+    background-color: #e5e7eb;
 }
 
 .hover\:bg-gray-300:hover {
-  background-color: #d1d5db;
+    background-color: #d1d5db;
 }
 
 /* 댓글 부분 */
 .comment-section {
-  margin-top: 1rem;
+    margin-top: 1rem;
 }
 
 .comment-count {
-  font-weight: 600;
+    font-weight: 600;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -17,39 +17,39 @@ import ProjectWrite from "@/pages/Project/ProjectWrite.vue";
 import TeamList from "@/pages/team/TeamList.vue";
 import TeamDeteil from "@/pages/team/TeamDeteil.vue";
 import TeamWrite from "@/pages/team/TeamWrite.vue";
-import ReportCreate from '@/pages/report/ReportCreate.vue';
-import ReportList from '@/pages/report/ReportList.vue';
-import ReportDetails from '@/pages/report/ReportDetails.vue';
+import ReportCreate from "@/pages/report/ReportCreate.vue";
+import ReportList from "@/pages/report/ReportList.vue";
+import ReportDetails from "@/pages/report/ReportDetails.vue";
 import CommentList from "@/pages/Post/Comment/CommentList.vue";
 import CommentCreate from "@/pages/Post/Comment/CommentCreate.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {path: "/", component: Home},
-    {path: "/login", component: Login},
-    {path: "/users", component: Users},
-    {path: "/admins", component: Admins},
-    {path: "/about", component: About},
-    {path: "/post", component: PostList},
-    {path: "/post/:postNo", component: PostDetail},
-    {path: "/post/write", name: "PostWrite", component: PostWrite},
-    {path: '/posts/:postNo/comments', name: 'CommentList', component: CommentList },
-    {path: '/posts/:postNo/comments/create', name: 'CommentCreate', component: CommentCreate },
-    {path: "/messages/list", component: MessageList},
-    {path: "/messages/write", component: MessageCreate},
-    {path: "/messages/:messageNo", component: MessageDetails},
-    {path: "/team", name: "TeamList", component: TeamList},
-    {path: "/team/:teamtNo", name: "TeamDetailt", component: TeamDeteil},
-    {path: "/team/write", name: "TeamWrite", component: TeamWrite},
-    { path: '/reports/write', component: ReportCreate},
-    { path: '/reports/list', component: ReportList},
-    { path: '/reports/:reportNo', component: ReportDetails },
-    {path: "/project", name: "ProjectList", component: ProjectList},
-    {path: "/project/:projectNo", name: "ProjectDetail", component: ProjectDetail},
-    {path: "/project/write", name: "ProjectWrite", component: ProjectWrite},
-    {path: "/:paths(.*)*", component: NotFound},
-  ],
+    history: createWebHistory(),
+    routes: [
+        {path: "/", component: Home},
+        {path: "/login", component: Login},
+        {path: "/users", component: Users},
+        {path: "/admins", component: Admins},
+        {path: "/about", component: About},
+        {path: "/post", component: PostList},
+        {path: "/post/:postNo", component: PostDetail},
+        {path: "/post/write", name: "PostWrite", component: PostWrite},
+        {path: "/posts/:postNo/comments", name: "CommentList", component: CommentList},
+        {path: "/posts/:postNo/comments/create", name: "CommentCreate", component: CommentCreate},
+        {path: "/messages/list", component: MessageList},
+        {path: "/messages/write", component: MessageCreate},
+        {path: "/messages/:messageNo", component: MessageDetails},
+        {path: "/team", name: "TeamList", component: TeamList},
+        {path: "/team/:teamNo", name: "TeamDetailt", component: TeamDeteil},
+        {path: "/team/write", name: "TeamWrite", component: TeamWrite},
+        {path: "/reports/write", component: ReportCreate},
+        {path: "/reports/list", component: ReportList},
+        {path: "/reports/:reportNo", component: ReportDetails},
+        {path: "/project", name: "ProjectList", component: ProjectList},
+        {path: "/project/:projectNo", name: "ProjectDetail", component: ProjectDetail},
+        {path: "/project/write", name: "ProjectWrite", component: ProjectWrite},
+        {path: "/:paths(.*)*", component: NotFound},
+    ],
 });
 
 export default router;
